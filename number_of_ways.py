c67044ccb583d2a8942f4eae1780d43903b8105c
Martin Ma <z74ma@uwaterloo.ca>
2022-09-13 00:07:23 -0400

    Fix error
    Fix error in loop iteration.

--- number_of_ways.py
+++ number_of_ways.py
@@ -10,28 +10,28 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
     Initially, you are standing at position startPos on an infinite
     number line. With one step, you can move either one position to the left,
     or one position to the right.
-    
+     
     Given a positive integer k, return the number of different ways to
     reach the position endPos starting from startPos, such that you
     perform exactly k steps.
     """
     # start with path of length 1
-    paths = [startPos]
+    paths = [[startPos]]
 
     # loop k times
     for i in tqdm(range(k)):
-        for path in paths:
-            new_path = path.copy()
+        for _ in range(len(paths)):
+            new_path = paths.pop(0)
             last_position = new_path[-1]
 
             # exist fast if not going to make to end
-            if endPos - last_position > (k - i - 1):
+            if endPos - last_position > (k - i):
                 continue
             # path that goes to the left
             new_path_left = new_path + [last_position - 1]
 
             # path that goes to the right
-            new_path_right = new_path + [last_position - 1]
+            new_path_right = new_path + [last_position + 1]
 
             # add paths to the left and right
             paths.append(new_path_left)
